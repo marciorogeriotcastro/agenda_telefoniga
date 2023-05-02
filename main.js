921d4d5ecd5ef369af04beaa5adc34489c81b6df
Marcio de Castro <marcio7109@gmail.com>
2023-05-02 19:25:00 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('form-contatos');
+let linhas = '';
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-telefone');
+
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputNumeroContato.value}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+});
